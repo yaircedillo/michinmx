e862f1ef7ef355bb3d92d3f7c914687b49597c68
Yair Cedillo Jaramillo <al221610420@gmail.com>
2018-10-16 23:28:04 -0500

    se revisaron archivos

--- app/municipios.php
+++ app/municipios.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class municipios extends Model
 {
     protected $table = 'municipio';
+    protected $primaryKey = 'id_municipio';
     protected $fillable=['id_municipio','municipio','id_estado'];
 }
--- database/migrations/2018_10_05_153921_municipio.php
+++ database/migrations/2018_10_05_153921_municipio.php
@@ -9,19 +9,16 @@ class Municipio extends Migration
     public function up()
     {
         Schema::create('municipio', function (Blueprint $table) {
-            $table->increments('id_municipio');
-		    $table->string('municipio',40);
-			$table->integer('id_estado')->unsigned();
-            $table->integer('id_puesto')->unsigned();
+            $table->increments('id_municipios');
+            $table->string('municipio',40);
+            $table->integer('id_estado')->unsigned();
             $table->rememberToken();
 		    $table->timestamps();
             $table->foreign('id_estado')->references('id_estado')->on('estado');
-		    $table->foreign('id_puesto')->references('id_puesto')->on('puesto');		
+		   		
 			
         });
     }
-
-    
     public function down()
     {
         Schema::drop('municipio');
